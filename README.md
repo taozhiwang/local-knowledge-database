1f4277692943527d424cbd1dfb2c4bd219161e81
taozhiwang <taozhiwa@gmail.com>
2023-06-27 12:54:34 +0800

    Azure openai version improvements Add README.md

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Local knowledge Base
+# 基于本地知识库的问答生成
+## Demo
+原理类似[langchain-ChatGLM](https://github.com/imClumsyPanda/langchain-ChatGLM/tree/master)中所展示的流程图
+
+具体实现上，采用UnstructuredFileLoader读入文档，文档支持txt，pdf，docs等数据格式
+读入文档后，采用CharacterTextSplitter进行切分
+
+Embedding选择HuggingFaceEmbeddings
+- SentenceTransformer不支持arm架构
+- openai Embedding 需要消耗tokens
+
+向量数据库储存和查找相似值采用FAISS
+- awadb所需格式不匹配
+
+随后采用Azure openai serves得到答案
+- 也可以用FastChat或者ChatGLM-6B
+
+### 样例
+样例路径: ./data
+样例场景为一个淘宝商家的问答bot
+用户输入: 这款衣服的袖型是什么
+system prompt: 你是一个根据提供的材料作出回答的助手
+user prompt: ./data/test-prompt.txt
+Output: 这款衣服的袖型是收口袖。
--- main.py
+++ main.py
@@ -20,39 +20,43 @@ split_docs = text_splitter.split_documents(data)
 docs = split_docs
 print("split_docs size:",len(split_docs))
 
-#apikey = os.getenv('OPENAI_API_KEY')
-#embeddings = OpenAIEmbeddings(key = apikey)
-#print(embeddings)
-
 # Embedding with hugging face hub
 embeddings = HuggingFaceEmbeddings()
 
+# Store and search by using FAISS
 db = FAISS.from_documents(split_docs, embeddings)
-query = "衣服的袖长是什么"
+query = "这款衣服的袖型是什么"
 similar_docs = db.similarity_search(query)
 
 # Create prompt
-prompt = f"已知信息：\n{similar_docs[0].page_content}\n{similar_docs[1].page_content}请根据这些信息回答问题：\n{query}"
- 
+prompt = f"已知信息：\n{similar_docs[0].page_content}\n{similar_docs[1].page_content}\n请根据这些信息回答问题:\n{query}\n请仅对上述问题作出回答, 不要包含无关信息\n"
+
+# Openai api setting
 openai.api_key = os.environ["OPENAI_API_KEY"]
 openai.api_base = os.environ["AZURE_OPENAI_ENDPOINT"]
 print(openai.api_base)
-deployment_name = 'Breadcrumbsautomatic-assessment-generation'
-response = openai.Completion.create(engine=deployment_name, prompt=prompt, max_tokens=200)
+deployment_name = "Breadcrumbsautomatic-assessment-generation"
+openai.api_version = "2023-05-15" # this may change in the future
+openai.api_type = "azure"
+#response = openai.Completion.create(model="gpt-3.5-turbo",engine=deployment_name, prompt=prompt, max_tokens=20)
 
-'''
+# Create response from gpt-3.5
 response = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
+  engine=deployment_name,
   temperature =  0.7,
   messages=[
+        {"role": "system", "content": "你是一个根据提供的材料作出回答的助手"},
         {"role": "user", "content": prompt},
-    ]
+    ],
+  max_tokens=20
 )
-'''
+
 
 if __name__ == "__main__":
   print(split_docs[0])
   print(split_docs[1])
   print(similar_docs[0])
   print(prompt)
-  print(text = response['choices'][0]['text'].replace('\n', '').replace(' .', '.').strip())
+  print(response)
+  print(response['choices'][0]['message']['content'].replace(' .', '.').strip())
