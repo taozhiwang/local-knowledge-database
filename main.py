b9f55f1a5b725e561c6db7ecd99fdf389238873f
taozhiwang <taozhiwa@gmail.com>
2023-06-27 14:12:53 +0800

    Use awadb instead of FAISS for vector database

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from langchain.text_splitter import CharacterTextSplitter
 #from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import AwaDB
 import os
 import openai
+import awadb
 
 # Import text
 loader = UnstructuredFileLoader("./data/test.txt")
@@ -17,16 +19,20 @@ print(f'documents:{len(data)}')
 text_splitter = CharacterTextSplitter(chunk_size=25, chunk_overlap=5)
 # Split the document
 split_docs = text_splitter.split_documents(data)
-docs = split_docs
 print("split_docs size:",len(split_docs))
 
 # Embedding with hugging face hub
 embeddings = HuggingFaceEmbeddings()
 
 # Store and search by using FAISS
-db = FAISS.from_documents(split_docs, embeddings)
+#db = FAISS.from_documents(split_docs, embeddings)
 query = "这款衣服的袖型是什么"
-similar_docs = db.similarity_search(query)
+#similar_docs = db.similarity_search(query)
+
+# Store and search by using awadb
+db = AwaDB.from_documents(split_docs)
+docs = db.similarity_search(query)
+
 
 # Create prompt
 prompt = f"已知信息：\n{similar_docs[0].page_content}\n{similar_docs[1].page_content}\n请根据这些信息回答问题:\n{query}\n请仅对上述问题作出回答, 不要包含无关信息\n"
@@ -41,6 +47,7 @@ openai.api_type = "azure"
 #response = openai.Completion.create(model="gpt-3.5-turbo",engine=deployment_name, prompt=prompt, max_tokens=20)
 
 # Create response from gpt-3.5
+'''
 response = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
   engine=deployment_name,
@@ -51,12 +58,12 @@ response = openai.ChatCompletion.create(
     ],
   max_tokens=20
 )
-
+'''
 
 if __name__ == "__main__":
   print(split_docs[0])
   print(split_docs[1])
   print(similar_docs[0])
   print(prompt)
-  print(response)
-  print(response['choices'][0]['message']['content'].replace(' .', '.').strip())
+  #print(response)
+  #print(response['choices'][0]['message']['content'].replace(' .', '.').strip())
